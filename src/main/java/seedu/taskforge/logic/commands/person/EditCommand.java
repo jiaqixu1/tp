3e5a63ebdf4c55e117b3bf2482f6e80e634243da
Hsu Myat Noe <hsumyatnoe134@gmail.com>
2026-04-02 16:22:13 +0800

    Fix checkstyle errors

--- src/main/java/seedu/taskforge/logic/commands/person/EditCommand.java
+++ src/main/java/seedu/taskforge/logic/commands/person/EditCommand.java
@@ -52,8 +52,8 @@ public class EditCommand extends Command {
 
     public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
-    public static final String MESSAGE_DUPLICATE_PERSON = "Another person with the " +
-            "same name, phone, or email already exists in taskforge";
+    public static final String MESSAGE_DUPLICATE_PERSON = "Another person with the "
+            + "same name, phone, or email already exists in taskforge";
 
     private final Index index;
     private final EditPersonDescriptor editPersonDescriptor;

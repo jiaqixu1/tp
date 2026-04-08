c96019f04c150ca6f8a9431c511dd972f99e3729
Hsu Myat Noe <hsumyatnoe134@gmail.com>
2026-04-08 19:23:47 +0800

    Fix task edit command bug

--- src/test/java/seedu/taskforge/logic/commands/task/EditTaskCommandTest.java
+++ src/test/java/seedu/taskforge/logic/commands/task/EditTaskCommandTest.java
@@ -1,25 +1,27 @@
 package seedu.taskforge.logic.commands.task;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.taskforge.logic.commands.CommandTestUtil.VALID_PROJECT_ALPHA;
 import static seedu.taskforge.logic.commands.CommandTestUtil.VALID_TASK_FIX_ERROR;
 import static seedu.taskforge.logic.commands.CommandTestUtil.VALID_TASK_IMPLEMENT_X;
 import static seedu.taskforge.logic.commands.CommandTestUtil.VALID_TASK_REFACTOR;
 import static seedu.taskforge.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.taskforge.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.taskforge.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.taskforge.testutil.TypicalIndexes.INDEX_FIRST_TASK;
 import static seedu.taskforge.testutil.TypicalPersons.getTypicalAddressBook;
-
-import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.taskforge.commons.core.index.Index;
+import seedu.taskforge.logic.Messages;
 import seedu.taskforge.model.AddressBook;
 import seedu.taskforge.model.Model;
 import seedu.taskforge.model.ModelManager;
 import seedu.taskforge.model.UserPrefs;
+import seedu.taskforge.model.person.Person;
+import seedu.taskforge.model.person.PersonTask;
 import seedu.taskforge.model.project.Project;
 import seedu.taskforge.model.task.Task;
 
@@ -29,37 +31,57 @@ public class EditTaskCommandTest {
 
     @Test
     public void execute_editTask_success() {
-        Project targetProject = new Project(VALID_PROJECT_ALPHA);
-
         Task newTask = new Task(VALID_TASK_IMPLEMENT_X);
-        EditTaskCommand editTaskCommand = new EditTaskCommand(targetProject, INDEX_FIRST_TASK, newTask);
+        EditTaskCommand editTaskCommand = new EditTaskCommand(INDEX_FIRST_PERSON, INDEX_FIRST_TASK, newTask);
 
-        Project expectedEditedProject = new Project(VALID_PROJECT_ALPHA,
-                Arrays.asList(new Task(VALID_TASK_IMPLEMENT_X), new Task(VALID_TASK_FIX_ERROR)));
+        Person expectedPerson = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        PersonTask expectedPersonTask = expectedPerson.getTasks().get(INDEX_FIRST_TASK.getZeroBased());
+        Project expectedProjectInModel = model.getProjectList().get(expectedPersonTask.getProjectIndex());
+        Task taskToEdit = expectedProjectInModel.getTasks().get(expectedPersonTask.getTaskIndex());
+        Task renamedTaskWithProject = new Task(VALID_TASK_IMPLEMENT_X, expectedProjectInModel.title);
+        if (taskToEdit.getStatus()) {
+            renamedTaskWithProject.setDone();
+        }
+
+        Project expectedEditedProject = new Project(expectedProjectInModel.title, expectedProjectInModel.getTasks());
+        expectedEditedProject.getUniqueTaskList().setTask(taskToEdit, renamedTaskWithProject);
 
         String expectedMessage = String.format(EditTaskCommand.MESSAGE_SUCCESS, expectedEditedProject);
 
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
-        Project expectedProjectInModel = expectedModel.getProjectList().stream()
-                .filter(project -> project.equals(targetProject))
-                .findFirst()
-                .orElseThrow();
-        expectedModel.setProject(expectedProjectInModel, expectedEditedProject);
+        Person copiedExpectedPerson = expectedModel.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        PersonTask copiedExpectedPersonTask = copiedExpectedPerson.getTasks().get(INDEX_FIRST_TASK.getZeroBased());
+        Project copiedExpectedProject = expectedModel.getProjectList().get(copiedExpectedPersonTask.getProjectIndex());
+        expectedModel.setProject(copiedExpectedProject, expectedEditedProject);
 
         assertCommandSuccess(editTaskCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
-    public void execute_projectNotFound_failure() {
-        EditTaskCommand editTaskCommand = new EditTaskCommand(new Project("nonexistent"), INDEX_FIRST_TASK,
+    public void execute_editTask_keepsPersonTaskAssignment() throws Exception {
+        Person personBeforeEdit = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        int taskCountBeforeEdit = personBeforeEdit.getTasks().size();
+
+        EditTaskCommand editTaskCommand = new EditTaskCommand(INDEX_FIRST_PERSON, INDEX_FIRST_TASK,
+                new Task(VALID_TASK_IMPLEMENT_X));
+        editTaskCommand.execute(model);
+
+        Person personAfterEdit = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        assertEquals(taskCountBeforeEdit, personAfterEdit.getTasks().size());
+    }
+
+    @Test
+    public void execute_invalidPersonIndex_failure() {
+        Index outOfBoundPersonIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        EditTaskCommand editTaskCommand = new EditTaskCommand(outOfBoundPersonIndex, INDEX_FIRST_TASK,
                 new Task(VALID_TASK_IMPLEMENT_X));
 
-        assertCommandFailure(editTaskCommand, model, EditTaskCommand.MESSAGE_PROJECT_NOT_FOUND);
+        assertCommandFailure(editTaskCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
 
     @Test
     public void execute_taskIndexOutOfBound_failure() {
-        EditTaskCommand editTaskCommand = new EditTaskCommand(new Project(VALID_PROJECT_ALPHA),
+        EditTaskCommand editTaskCommand = new EditTaskCommand(INDEX_FIRST_PERSON,
                 Index.fromOneBased(99), new Task(VALID_TASK_IMPLEMENT_X));
 
         assertCommandFailure(editTaskCommand, model, EditTaskCommand.MESSAGE_INDEX_OUT_OF_BOUND);
@@ -67,7 +89,7 @@ public class EditTaskCommandTest {
 
     @Test
     public void execute_duplicateTaskNameInProject_failure() {
-        EditTaskCommand editTaskCommand = new EditTaskCommand(new Project(VALID_PROJECT_ALPHA), INDEX_FIRST_TASK,
+        EditTaskCommand editTaskCommand = new EditTaskCommand(INDEX_FIRST_PERSON, INDEX_FIRST_TASK,
                 new Task(VALID_TASK_FIX_ERROR));
 
         assertCommandFailure(editTaskCommand, model, EditTaskCommand.MESSAGE_DUPLICATE_TASK);
@@ -75,11 +97,11 @@ public class EditTaskCommandTest {
 
     @Test
     public void equals() {
-        EditTaskCommand firstCommand = new EditTaskCommand(new Project(VALID_PROJECT_ALPHA), INDEX_FIRST_TASK,
+        EditTaskCommand firstCommand = new EditTaskCommand(INDEX_FIRST_PERSON, INDEX_FIRST_TASK,
                 new Task(VALID_TASK_IMPLEMENT_X));
-        EditTaskCommand sameValuesCommand = new EditTaskCommand(new Project(VALID_PROJECT_ALPHA), INDEX_FIRST_TASK,
+        EditTaskCommand sameValuesCommand = new EditTaskCommand(INDEX_FIRST_PERSON, INDEX_FIRST_TASK,
                 new Task(VALID_TASK_IMPLEMENT_X));
-        EditTaskCommand differentCommand = new EditTaskCommand(new Project(VALID_PROJECT_ALPHA), INDEX_FIRST_TASK,
+        EditTaskCommand differentCommand = new EditTaskCommand(INDEX_FIRST_PERSON, INDEX_FIRST_TASK,
                 new Task(VALID_TASK_REFACTOR));
 
         assertTrue(firstCommand.equals(firstCommand));

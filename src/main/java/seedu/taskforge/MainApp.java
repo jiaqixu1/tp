4cea822a5bbfa027b0c8c414b347741e10e4c030
jiaqixu1 <e1355232@u.nus.edu>
2026-04-14 00:09:43 +0800

    Update MainApp.java

--- src/main/java/seedu/taskforge/MainApp.java
+++ src/main/java/seedu/taskforge/MainApp.java
@@ -36,7 +36,7 @@ import seedu.taskforge.ui.UiManager;
  */
 public class MainApp extends Application {
 
-    public static final Version VERSION = new Version(1, 16, 0, false);
+    public static final Version VERSION = new Version(1, 6, 0, false);
 
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
